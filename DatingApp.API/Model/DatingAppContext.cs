54e06a6821ccac0aeb0a90c7b8d68b528b23932b
Aditya Kumar <avats218@gmail.com>
2020-08-01 21:25:07 +0530

    Initial Commit

---
+++ DatingApp.API/Model/DatingAppContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatingApp.API.Model
+{
+    public partial class DatingAppContext : DbContext
+    {
+        public DatingAppContext()
+        {
+        }
+
+        public DatingAppContext(DbContextOptions<DatingAppContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Value> Value { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\;Database=DatingApp;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.2-servicing-10034");
+
+            modelBuilder.Entity<Value>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(25)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
